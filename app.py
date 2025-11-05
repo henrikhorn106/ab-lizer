7f374d87493a2d8a4d345e972e6bfcd9f5df1edb
Henrik Horn <henrik.horn106@gmail.com>
2025-11-05 12:16:08 +0100

    Initialization of flask app

--- app.py
+++ app.py
@@ -12,7 +12,20 @@ The platform provides a clean API with full CRUD functionality,
 a connected database for test storage, and an AI layer that turns data into actionable recommendations.
 """
 
-# Route /
-"""
+import os
 
-"""
+from flask import Flask
+
+from models import db
+
+app = Flask(__name__)
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.join(basedir, 'data/database.db')}"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
